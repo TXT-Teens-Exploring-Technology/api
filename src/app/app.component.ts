2470aa8dbde46af111a04c23518e25c98b0cd960
roberto sanchez <roberto@urbantxt.com>
2019-06-26 21:30:31 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'api';
+  data;
+
+
+  constructor(public http: HttpClient){
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9bbfaccfac0f45cd13f1ba17e3cfaec7')
+    .subscribe((payload) => {
+      this.data = payload;
+      console.log(this.data);
+    }, (err) => {
+      console.error(err);
+    })
+  }
 }
